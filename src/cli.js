2381449d61d325978ccbad59e5dcb2c4b5f1473b
Brandon Correa <me@bwawan.com>
2026-04-01 18:06:52 -0500

    Refactor

--- src/cli.js
+++ src/cli.js
@@ -17,29 +17,66 @@ Options:
   --version, -v             Show version`
 
 function parseArgs(argv) {
-  const parsed = { dir: null, output: null, file: null, dependentsOf: null, dependenciesOf: null, help: false, version: false }
-  let i = 0
-  while (i < argv.length) {
+  const parsed = {}
+  for (let i = 0; i < argv.length; i++) {
     const arg = argv[i]
-    if (arg === '--help' || arg === '-h') { parsed.help = true }
-    else if (arg === '--version' || arg === '-v') { parsed.version = true }
-    else if (arg === '--output' || arg === '-o') { parsed.output = argv[++i] }
-    else if (arg === '--file') { parsed.file = argv[++i] }
-    else if (arg === '--dependents-of') { parsed.dependentsOf = argv[++i] }
-    else if (arg === '--dependencies-of') { parsed.dependenciesOf = argv[++i] }
-    else if (!arg.startsWith('-')) { parsed.dir = arg }
-    i++
+    if (arg === '--help' || arg === '-h')
+      parsed.help = true
+    else if (arg === '--version' || arg === '-v')
+      parsed.version = true
+    else if (arg === '--output' || arg === '-o')
+      parsed.output = argv[++i]
+    else if (arg === '--file')
+      parsed.file = argv[++i]
+    else if (arg === '--dependents-of')
+      parsed.dependentsOf = argv[++i]
+    else if (arg === '--dependencies-of')
+      parsed.dependenciesOf = argv[++i]
+    else if (!arg.startsWith('-'))
+      parsed.dir = arg
   }
   return parsed
 }
 
+function scanDependents(args, index) {
+  const filtered = {}
+  for (const [file, info] of Object.entries(index))
+    if (info.dependencies.includes(args.dependentsOf))
+      filtered[file] = info
+  return filtered
+}
+
+function scanFile(args, index) {
+  const entry = index[args.file]
+  return entry ? { [args.file]: entry } : {}
+}
+
+function scanDependencies(args, index) {
+  const entry = index[args.dependenciesOf]
+  return entry ? { [args.dependenciesOf]: entry.dependencies } : {}
+}
+
 function output(data, outputPath) {
   const json = JSON.stringify(data, null, 2) + '\n'
-  if (outputPath) {
+  if (outputPath)
     writeFileSync(outputPath, json)
-  } else {
+  else
     process.stdout.write(json)
-  }
+}
+
+async function doScan(args) {
+  const { scan } = await import('./scan.js')
+  const index = await scan(resolve(args.dir || '.'))
+  const writeOut = data => output(data, args.output)
+
+  if (args.file)
+    writeOut(scanFile(args, index))
+  else if (args.dependentsOf)
+    writeOut(scanDependents(args, index))
+  else if (args.dependenciesOf)
+    writeOut(scanDependencies(args, index))
+  else
+    writeOut(index)
 }
 
 async function main(argv) {
@@ -56,28 +93,7 @@ async function main(argv) {
     return 0
   }
 
-  const { scan } = await import('./scan.js')
-  const dir = resolve(args.dir || '.')
-  const index = await scan(dir)
-
-  if (args.file) {
-    const entry = index[args.file]
-    output(entry ? { [args.file]: entry } : {}, args.output)
-  } else if (args.dependentsOf) {
-    const filtered = {}
-    for (const [file, info] of Object.entries(index)) {
-      if (info.dependencies.includes(args.dependentsOf)) {
-        filtered[file] = info
-      }
-    }
-    output(filtered, args.output)
-  } else if (args.dependenciesOf) {
-    const entry = index[args.dependenciesOf]
-    output(entry ? { [args.dependenciesOf]: entry.dependencies } : {}, args.output)
-  } else {
-    output(index, args.output)
-  }
-
+  await doScan(args)
   return 0
 }
 
